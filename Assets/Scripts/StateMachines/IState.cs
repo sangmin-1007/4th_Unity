aa421aa3dbf0f60a6553ae4b223f9192a6418480
sangmin-1007 <dosin0822@gmail.com>
2024-02-19 15:04:44 +0900

    Feat : Player 준비, StateMachine 인터페이스,추상클래스 준비

--- Assets/Scripts/StateMachines/IState.cs
+++ Assets/Scripts/StateMachines/IState.cs
@@ -1,18 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class IState : MonoBehaviour
+public interface IState
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public void Enter();
+    public void Exit();
+    public void HandleInput();
+    public void Update();
+    public void PhsicsUpdate();
 }
--- Assets/Scripts/StateMachines/StateMachine.cs
+++ Assets/Scripts/StateMachines/StateMachine.cs
@@ -2,17 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StateMachine : MonoBehaviour
+public abstract class StateMachine
 {
-    // Start is called before the first frame update
-    void Start()
+    protected IState currentState;
+
+    public void ChangeState(IState newState)
     {
-        
+        currentState?.Exit();
+
+        currentState = newState;
+
+        currentState.Enter();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void HandleInput()
     {
-        
+        currentState?.HandleInput();
+    }
+
+    public void Update()
+    {
+        currentState?.Update();
+    }
+    public void PhysicsUpdate()
+    {
+        currentState?.PhsicsUpdate();
     }
 }
