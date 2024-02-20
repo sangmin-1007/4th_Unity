4f6948cf494ffe4a2dde4c38e107e9242088cf29
sangmin-1007 <dosin0822@gmail.com>
2024-02-20 12:11:32 +0900

    Feat : Enemy 움직임 구현
    Enemy가 Player를 따라다니면서 공격하도록 구현

--- Assets/Scripts/Characters/Player/PlayerAnimationData.cs
+++ Assets/Scripts/Characters/Player/PlayerAnimationData.cs
@@ -17,6 +17,7 @@ public class PlayerAnimationData
     [SerializeField] private string fallParameterName = "Fall";
 
     [SerializeField] private string attackParameterName = "@Attack";
+    [SerializeField] private string baseAttackParameterName = "BaseAttack";
     [SerializeField] private string comboAttackParameterName = "ComboAttack";
 
     public int GroundParameterHash {  get; private set; }
@@ -30,6 +31,7 @@ public class PlayerAnimationData
 
     public int AttackParameterHash { get; private set; }
     public int ComboAttackParameterHash { get; private set; }
+    public int BaseAttackParameterHash { get; private set; }
 
 
     public void Initialize()
@@ -45,5 +47,6 @@ public class PlayerAnimationData
 
         AttackParameterHash = Animator.StringToHash(attackParameterName);
         ComboAttackParameterHash = Animator.StringToHash(comboAttackParameterName);
+        BaseAttackParameterHash = Animator.StringToHash(baseAttackParameterName);
     }
 }
